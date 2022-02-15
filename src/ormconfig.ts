b598e785ae8a0a2a564b17d6c100195930edf744
kerlos1256 <kero24862@gmail.com>
2022-02-15 09:21:00 +0200

    db url

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,11 +1,13 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 export const config: PostgresConnectionOptions = {
-  type: 'postgres',
-  port: 5432,
-  database: 'ytdl',
-  username: 'postgres',
-  password: '123456',
-  synchronize: true,
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  type:'postgres',
+  url: process.env.DATABASE_URL
+  // type: 'postgres',
+  // port: 5432,
+  // database: 'ytdl',
+  // username: 'postgres',
+  // password: '123456',
+  // synchronize: true,
+  // entities: ['dist/**/*.entity{.ts,.js}'],
 };
