aa7888767f9bbf99db39325f7f939834d0d16cd5
kerlos1256 <kero24862@gmail.com>
2022-02-15 10:04:34 +0200

    Port

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ async function bootstrap() {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(cookieParser())
-  
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
